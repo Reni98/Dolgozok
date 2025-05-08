9cf0d63a93b741d1aaec4f69e75b5c86547f081a
Reni98 <93825160+Reni98@users.noreply.github.com>
2025-05-08 10:35:35 +0200

    Add files via upload

---
+++ Dolgozok.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolgozok
+{
+    internal class Dolgozok
+    {
+        public string nev;
+        public int kor;
+        public string nem;
+        public string pozicio;
+        public string reszleg;
+
+        public Dolgozok(string nev, string kor, string nem, string pozicio, string reszleg)
+        {
+            this.nev = nev;
+            this.kor = int.Parse(kor);
+            this.nem = nem;
+            this.pozicio = pozicio;
+            this.reszleg = reszleg;
+        }
+    }
+}
